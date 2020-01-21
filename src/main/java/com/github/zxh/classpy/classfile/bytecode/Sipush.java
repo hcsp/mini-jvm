43aa9e6d9cad8f922f5cce62e24d1cad0fa95279
JayChenFE <jaychenfe@foxmail.com>
2020-01-21 23:11:38 +0800

    添加指令 (#1)
    添加指令 (#1)

--- src/main/java/com/github/zxh/classpy/classfile/bytecode/Sipush.java
+++ src/main/java/com/github/zxh/classpy/classfile/bytecode/Sipush.java
@@ -14,5 +14,9 @@ public class Sipush extends Instruction {
         short operand = reader.readShort();
         setDesc(getDesc() + " " + operand);
     }
+
+    public short getOperand() {
+        return Short.parseShort(getDesc().substring("sipush ".length()));
+    }
     
 }
